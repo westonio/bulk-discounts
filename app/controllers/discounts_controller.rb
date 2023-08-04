919a16f8050152dcad35a5fccb31c41e391ef702
Weston Schutt <wtschutt@gmail.com>
2023-08-04 07:15:45 -0600

    Feature: Add route and controller actions for creating New Discounts

--- app/controllers/discounts_controller.rb
+++ app/controllers/discounts_controller.rb
@@ -1,9 +1,42 @@
 class DiscountsController < ApplicationController
+  before_action :find_merchant_and_discount, only: [:show]
+  before_action :find_merchant, only: [:index, :new, :create]
+
   def index
-    @merchant = Merchant.find(params[:merchant_id])
+
   end
 
   def show
-    @invoice = Invoice.find(params[:invoice_id])
+
+  end
+
+  def new
+    @discount = Discount.new
+  end
+
+  def create
+    discount = Discount.new(discount_params)
+    if discount.save
+      redirect_to merchant_discounts_path(@merchant)
+      flash[:success] = "Discount Successfully Added!" 
+    else
+      flash[:danger] = "Discount Not created: Required information missing"
+      render :new
+    end
+  end
+
+private
+
+  def discount_params
+    params.require(:discount).permit(:percent_discount, :threshold_quantity, :merchant_id)
+  end
+
+  def find_merchant
+    @merchant = Merchant.find(params[:merchant_id])
+  end
+
+  def find_merchant_and_discount
+    @merchant = Merchant.find(params[:merchant_id])
+    @discount = Discount.find(params[:id])
   end
 end
