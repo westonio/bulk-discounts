10b2f2672af6f36d7d02fec9e334036b74d7688f
Weston Schutt <wtschutt@gmail.com>
2023-08-06 13:38:42 -0600

    Feature: Add #find_max_percent and #discount_applied methods for InvoiceItem model

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -6,12 +6,25 @@ class InvoiceItem < ApplicationRecord
                         :status
 
   belongs_to :invoice
-  belongs_to :item
-
+  belongs_to :item 
   enum status: [:pending, :packaged, :shipped]
 
   def self.incomplete_invoices
     invoice_ids = InvoiceItem.where("status = 0 OR status = 1").pluck(:invoice_id)
     Invoice.order(created_at: :asc).find(invoice_ids)
+  end
+
+  def find_max_percent
+    ii = InvoiceItem
+      .joins(item: { merchant: :discounts })
+      .select("MAX(discounts.percent_discount) as max_percent")
+      .where("discounts.threshold_quantity <= invoice_items.quantity")
+      .find_by(id: self.id)
+
+    ii.max_percent
+  end
+
+  def discount_applied
+    item.discounts.find_by(percent_discount: find_max_percent)
   end
 end
