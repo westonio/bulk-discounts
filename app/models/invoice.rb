afc15b84d2aa3164f9d82f972fff2c9282745444
Weston Schutt <wtschutt@gmail.com>
2023-08-06 09:55:28 -0600

    Feature: Add discount instance methods to Invoice model and add discounts association to Item model

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -7,10 +7,28 @@ class Invoice < ApplicationRecord
   has_many :invoice_items
   has_many :items, through: :invoice_items
   has_many :merchants, through: :items
+  has_many :discounts, through: :merchants
 
   enum status: [:cancelled, :in_progress, :completed]
 
   def total_revenue
     invoice_items.sum("unit_price * quantity")
+  end
+
+  
+  def total_discount
+    max_discounts = invoice_items.select("invoice_items.id, 
+      MAX(discounts.percent_discount / 100.0) 
+      * invoice_items.unit_price 
+      * invoice_items.quantity as total_discount")
+      .joins(item: { merchant: :discounts })
+      .where("discounts.threshold_quantity <= invoice_items.quantity")
+      .group("invoice_items.id")
+
+    max_discounts.sum { |invoice_item| invoice_item.total_discount }  
+  end
+    
+  def total_discounted_revenue
+    total_revenue - total_discount
   end
 end
